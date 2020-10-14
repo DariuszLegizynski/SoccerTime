839219e64f37d8e15c67d2f861cc191ac1a9fc9e
Dariusz Stefan LEGIZYNSKI <Legizynski@in-vision.at>
2020-10-14 08:28:34 +0200

    added firebase to apis and store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { firebaseReducer } from "react-redux-firebase";
 import allCountriesReducer from './allCountries/allCountriesReducer';
 import allLeaguesReducer from "./allLeagues/allLeagues/allLeaguesReducer";
 import leagueReducer from "./allLeagues/league/leagueReducer";
@@ -10,6 +11,7 @@ import NextLeagueEventsReducer from "./allLeagues/league/nextLeagueEventsReducer
 import PreviousLeagueEventsReducer from "./allLeagues/league/previousLeagueEventsReducer";
 
 const rootReducer = combineReducers({
+    firebaseReducer,
     allCountries: allCountriesReducer,
     allLeagues: allLeaguesReducer,
     league: leagueReducer,
--- src/store.js
+++ src/store.js
@@ -2,8 +2,14 @@ import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from "redux-devtools-extension";
 
+import firebase from "./apis/firebase";
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+
 import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(thunk.withExtraArgument({getFirebase})),
+    reactReduxFirebase(firebase)
+    ));
 
 export default store;
