372502d5d0a214b2ae1b0bab8c9e82502967f6a8
Dariusz Stefan LEGIZYNSKI <Legizynski@in-vision.at>
2020-11-13 23:30:12 +0100

    made the images in leagues show with dynamic span (using grid). Styled a bit the beginning of the league page.

---
+++ src/components/allLeagues/league/LeagueItem.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import _ from "lodash";
+
+import "./league.css";
+
+const LeagueItem = ({picture, altPicture, altText}) => {
+    const [ newSpanSize, setNewSpanSize ] = useState(0);
+
+    let imageHeight;
+    
+    const handleImageLoad = (event) => {
+        imageHeight = event.currentTarget.clientHeight;
+        if(_.isEmpty(imageHeight)) {
+            setNewSpanSize(Math.ceil(imageHeight / 10 + 1));
+        }
+    }
+
+    return (
+        <figure className="league__gallery__image" style={{ gridRowEnd: `span ${newSpanSize}`}}>
+            <img className="league__picture" onLoad={handleImageLoad} src={picture ? picture : altPicture} alt={altText} />
+        </figure>
+    )
+}
+
+export default LeagueItem;
--- src/components/allLeagues/league/league.js
+++ src/components/allLeagues/league/league.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -15,10 +15,9 @@ import PreviousLeagueEvents from "./previousLeagueEvents";
 
 import altFanart1 from "../../../resources/images/hero/training-1306131.jpg";
 import "./league.css";
+import LeagueItem from "./LeagueItem";
 
 const League = (props) => {
-    const [ newSpan, setNewSpan ] = useState(0);
-
     const leagueId = props.match.params.idLeague;
     const dispatch = useDispatch();
     const selectLeague = useSelector(state => state.league);
@@ -29,49 +28,19 @@ const League = (props) => {
         dispatch(getLeagueId(leagueId));
     }, [dispatch, leagueId]);
 
-    let imageHeight;
-    
-    const handleImageLoad = (event) => {
-        imageHeight = event.currentTarget.clientHeight;
-        console.log(imageHeight);
-        if(_.isEmpty(imageHeight)) {
-            // setNewSpan(Math.ceil(imageHeight / 10 + 1));
-            console.log(imageHeight);
-            console.log(newSpan);
-        }
-    }
-    console.log(newSpan);
-    // style={{gridRowEnd: `span ${newSpan}`}}
-
     const leagueIntro = () => {
         if(!_.isEmpty(selectLeagueId)) {
             return selectLeagueId.map(el => {
                 return (
-                    <section className="league__intro fadeIn" key={shortid.generate()}>
-                        <figure className="league__intro__images">
-                            <img className="league__fanart1" onLoad={handleImageLoad} src={el.strFanart1 ? el.strFanart1 + "/preview" : altFanart1} alt="Fanart1"/>
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__badge" onLoad={handleImageLoad} src={el.strBadge+"/preview"} alt="League-Badge" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__fanart2" onLoad={handleImageLoad} src={el.strFanart2+"/preview"} alt="Fanart2" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__trophy" onLoad={handleImageLoad} src={el.strTrophy} alt="Trophy" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__fanart3" onLoad={handleImageLoad} src={el.strFanart3+"/preview"} alt="Fanart3" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__logo" onLoad={handleImageLoad} src={el.strLogo+"/preview"} alt="LeagueLogo" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__poster" onLoad={handleImageLoad} src={el.strPoster+"/preview"} alt="Poster" />
-                        </figure>
-                        <figure className="league__intro__images">
-                            <img className="league__fanart4"  onLoad={handleImageLoad} src={el.strFanart4+"/preview"} alt="Fanart4" />
-                        </figure>
+                    <section className="league__gallery fadeIn" key={shortid.generate()}>
+                        <LeagueItem picture={el.strLogo} altPicture={altFanart1} altText={"Logo"} />
+                        <LeagueItem picture={el.strFanart2} altPicture={altFanart1} altText={"Fanart2"} />
+                        <LeagueItem picture={el.strBadge} altPicture={altFanart1} altText={"Badge"} />
+                        <LeagueItem picture={el.strFanart3} altPicture={altFanart1} altText={"Fanart3"} />
+                        <LeagueItem picture={el.strTrophy} altPicture={altFanart1} altText={"Trophy"} />
+                        <LeagueItem picture={el.strFanart1} altPicture={altFanart1} altText={"Fanart1"} />
+                        <LeagueItem picture={el.strPoster} altPicture={altFanart1} altText={"Poster"} />
+                        <LeagueItem picture={el.strFanart4} altPicture={altFanart1} altText={"Fanart4"} />
                     </section>
                 )
             })
@@ -82,14 +51,14 @@ const League = (props) => {
         if(!_.isEmpty(selectLeagueId)) {
             return selectLeagueId.map(el => {
                 return (
-                    <section className="league__title" key={shortid.generate()}>
-                        <div className="league__title">
-                            <h1 className="league__h1 h1">
+                    <section className="league__header" key={shortid.generate()}>
+                        <div className="league__header__title">
+                            <h1 className="league__header__title__h1 h1">
                                     {el.strLeague}
                             </h1>
                         </div>
-                        <div className="league__banner">
-                            <img className="league__banner" src={el.strBanner} alt="League-Banner" />
+                        <div className="league__header__banner">
+                            <img className="league__header__banner__image" src={el.strBanner} alt="League-Banner" />
                         </div>
                     </section>
                 )
