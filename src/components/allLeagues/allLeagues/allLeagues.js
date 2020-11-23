be78dac52e1e6bdd1c987d176fba6cba4b314927
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2020-11-23 09:34:49 +0100

    the landing page is now fully responsive.

--- src/components/allLeagues/allLeagues/allLeagues.js
+++ src/components/allLeagues/allLeagues/allLeagues.js
@@ -86,7 +86,7 @@ const AllLeagues = () => {
         infinite: false,
         speed: 500,
         slidesToShow: 7,
-        slidesToScroll: 4,
+        slidesToScroll: 6,
         initialSlide: 0,
         arrows: true,
         nextArrow: <SampleNextArrow to="next" />,
@@ -94,30 +94,40 @@ const AllLeagues = () => {
         responsive:
         [
             {
+                breakpoint: 1400,
+                settings:
+                    {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    dots: true
+                    }
+                },
+            {
             breakpoint: 1024,
             settings:
                 {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                dots: false
+                }
+            },
+            {
+            breakpoint: 768,
+            settings:
+                {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2,
+                dots: false
                 }
             },
             {
             breakpoint: 600,
             settings:
                 {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
-                }
-            },
-            {
-            breakpoint: 480,
-            settings:
-                {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                dots: false
                 }
             }
         ]
