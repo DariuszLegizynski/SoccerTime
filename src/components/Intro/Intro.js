58dc87a7e5fbc9d8e5c640a65641b0894d29b8ce
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-01-14 21:48:04 +0100

    added small down arrows to the intro of the landing page to give info about the need of scrolling.

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,40 +1,48 @@
 import React from "react";
 
 import "./Intro.css";
-import bgVideo from "../../resources/video/webm/1300678_Abstract_huge_football_HD_BG.webm"
+import bgVideo from "../../resources/video/webm/1300678_Abstract_huge_football_HD_BG.webm";
 
 const Intro = () => {
+	const playVideo = () => {
+		return (
+			<div className="intro__video fadeIn">
+				<video
+					className="intro__video__content"
+					autoPlay
+					muted
+					loop
+				>
+					<source src={bgVideo} type="video/webm" />
+					Your browser is not supported
+				</video>
+			</div>
+		);
+	};
 
-    const playVideo = () => {
-        return (
-            <div className="intro__video fadeIn">
-                <video className="intro__video__content" autoPlay muted loop>
-                    <source src={bgVideo} type="video/webm" />
-                    Your browser is not supported
-                </video>
-            </div>
-        )
-    }
-
-    return (
-        <section className="intro">
-            {playVideo()}
-            <div className="intro__top fadeIn">
-                <div className="intro__top__skew">
-                    <h1 className="intro__h1">
-                        All You Ever Wanted <span className="intro__span span">to Know About Football</span>
-                    </h1>
-                </div>
-            </div>
-            <div className="intro__bot fadeIn">
-                <div className="intro__top__skew">
-                    <h2 className="intro__h2">
-                        Is Right Here
-                    </h2>
-                </div>
-            </div>
-        </section>
-    )
-}
+	return (
+		<section className="intro">
+			{playVideo()}
+			<div className="intro__top fadeIn">
+				<div className="intro__top__skew">
+					<h1 className="intro__h1">
+						All You Ever Wanted{" "}
+						<span className="intro__span span">
+							to Know About Football
+						</span>
+					</h1>
+				</div>
+			</div>
+			<div className="intro__bot fadeIn">
+				<div className="intro__top__skew">
+					<h2 className="intro__h2">Is Right Here</h2>
+					<span className="intro__h1">
+						&darr; (scroll) &darr;
+					</span>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default Intro;
